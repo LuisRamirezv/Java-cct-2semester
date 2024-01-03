59ed34a39bcc7b297145342c054ee0d8d549e78a
Luis Ramirez Vega <luisramirezvega@Luiss-MacBook-Air.local>
2024-01-03 17:51:35 +0000

    Adding changes, file Arrays

--- Java Arrays/Zombiekiller.java
+++ Java Arrays/Zombiekiller.java
@@ -10,13 +10,21 @@
 public class Zombiekiller {
 
     public static void main(String[] args) {
+        // Welcome to this programming.
+
         System.out.println("Welcome to our zombie killer program!");
 
         System.out.println(" ");
-        
+
+        // In a zombie apocalypsis we have to have something to deffens us, like a weapon...
+        // Creating three weapons inside our backpack list 
         String backpack[] = {"Shotgun", "Assault Rifle", "Sniper"};
+
+        // Different kind of zombies
         String zombies[] = {"Close-range zombie", "Mid-range zombie", "Long-range zombie"};
 
+        // Zombie damage points
+        int damageZombieHate[] = {23, 45, 65, 88, 22, 44, 66, 778, 81, 99, 1, 3, 5};
 
         System.out.println("backpack Items:");
         System.out.println("- " + backpack[0]);
@@ -29,5 +37,13 @@ public class Zombiekiller {
         System.out.println("- " + zombies[0]);
         System.out.println("- " + zombies[1]);
         System.out.println("- " + zombies[2]);
+
+        System.out.println(" ");
+
+        System.out.println("Zombie damage:");
+        System.out.println("- " + damageZombieHate[0]);
+        System.out.println("- " + damageZombieHate[4]);
+        System.out.println("- " + damageZombieHate[9]);
+
     }
 }
