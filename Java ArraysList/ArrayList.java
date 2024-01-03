3b3c4a345311a7404d9d30fadff08c9df04f2c4b
Luis Ramirez Vega <luisramirezvega@Luiss-MacBook-Air.local>
2024-01-03 18:29:24 +0000

    Creating new file ArrayList

---
+++ Java ArraysList/ArrayList.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ * 
+ * 
+ */
+
+import java.util.ArrayLists
+
+public class ArrayList{
+    public static void main(String[] args) {
+
+		String[] fruits = new String[4];
+		fruits[0] = "Mango";
+		fruits[1] = "Apple";
+		fruits[2] = "Strawberry";
+		fruits[3] = "Watermelon";
+		System.out.println(fruits);
+
+		ArrayLists fruitList = new ArrayLists();
+
+		fruitList.add("Mango");
+		fruitList.add("Apple");
+		fruitList.add("Strawberry");
+		fruitList.add("Watermelon");
+
+
+    }
+}
